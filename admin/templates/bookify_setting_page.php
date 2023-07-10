30a804f5791f3e982c493d83d8e508f7e4d87ea3
MehdiREXON <m.ghazanfari1384@gmail.com>
2023-07-10 20:34:56 +0330

    bookify_setting_page.php has been added in order to show something in setting page

--- admin/templates/bookify_setting_page.php
+++ admin/templates/bookify_setting_page.php
@@ -1,1 +1,14 @@
 <?php
+function display_admin_notice() {
+	$screen = get_current_screen();
+
+	// Check if the current screen is the desired plugin page
+	if ( $screen && $screen->id === 'your-plugin-page' ) {
+		?>
+        <div class="notice notice-info">
+            <p>Your admin notice message here.</p>
+        </div>
+		<?php
+	}
+}
+add_action( 'admin_notices', 'display_admin_notice' );
