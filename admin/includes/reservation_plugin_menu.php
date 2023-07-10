05c31df6ca19662f9878b227da3e01c322a179b8
amirmassoud zamani <amirmzamani23@gmail.com>
2023-07-10 12:44:39 +0330

    add menu pages

--- admin/includes/reservation_plugin_menu.php
+++ admin/includes/reservation_plugin_menu.php
@@ -36,7 +36,7 @@ function bookify_settings_page_markup(): void
 	?>
 	<div class="wrap">
 	  <h1><?php esc_html_e( get_admin_page_title() ); ?></h1>
-	  <p><?php esc_html_e( 'Some content.', 'wpplugin' ); ?></p>
+	  <p><?php esc_html_e( 'Some content.', 'bookify' ); ?></p>
   
 	</div>
 	<?php
@@ -52,7 +52,7 @@ function bookify_settings_subpage_markup(): void
 	?>
 	<div class="wrap">
 	  <h1><?php esc_html_e( get_admin_page_title() ); ?></h1>
-	  <p><?php esc_html_e( 'Some content.', 'wpplugin' ); ?></p>
+	  <p><?php esc_html_e( 'Some content.', 'bookify' ); ?></p>
   
 	</div>
 	<?php
